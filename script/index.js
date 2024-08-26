05a3eb71bb9639d2da78af6b063f8496650855f2
HevertonMonteiro <167649076+HevertonMonteiro@users.noreply.github.com>
2024-08-26 20:31:12 -0300

    NOVO LAYOUT DA PAGINA

--- script/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const navLinks = document.querySelectorAll('.nav-link');
-        navLinks.forEach(link => {
-            link.addEventListener('click', event => {
-                event.preventDefault();
-                const dialogBox = link.nextElementSibling;
-                dialogBox.classList.toggle('show');
-            });
-        });
